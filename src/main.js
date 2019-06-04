7012a1874998c2297a293d1697445cf30081ed50
Tessa Sullivan and Zach Weintraub <zach@>
2019-06-04 14:54:14 -0700

    Currency exchange with keyup and change function

--- src/main.js
+++ src/main.js
@@ -25,7 +25,49 @@ import './styles.css';
 //     });
 //   });
 
+
+function populateDropdowns() {
+  let request = new XMLHttpRequest();
+  const url = `https://api.exchangerate-api.com/v4/latest/USD`;
+
+  request.open("GET", url, true);
+  request.send();
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status == 200) {
+      const response = JSON.parse(this.responseText);
+      for (let key in response.rates) {
+        if (key != "USD") {
+          $('#fromCurrency').append(`<option>${key}</option>`);
+        }
+      }
+      for (let key in response.rates) {
+        if (key != "EUR") {
+          $('#toCurrency').append(`<option>${key}</option>`);
+        }
+      }
+    }
+  }
+}
+
+function convert(amount, currencyOne, currencyTwo) {
+  $('#currencyResult').empty();
+  let request = new XMLHttpRequest();
+  const url = `https://api.exchangerate-api.com/v4/latest/${currencyOne}`;
+  let result = 0;
+
+  request.open("GET", url, true);
+  request.send();
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status == 200) {
+      const response = JSON.parse(this.responseText);
+      result = (amount * response.rates[currencyTwo]).toFixed(2);
+      $('#currencyResult').append(`<p>${result}</p>`);
+    }
+  }
+}
+
 $(document).ready(function() {
+  //weather logic
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
@@ -48,4 +90,18 @@ $(document).ready(function() {
       $('.showTemp').text(`The temperature is ${temp} F.`);
     }
   });
+  //currency logic
+  populateDropdowns();
+  $('#fromAmount').keyup(function() {
+    if ($('#currencyInput').val() != "") {
+      convert($('#fromAmount').val(), $('#fromCurrency').val(), $('#toCurrency').val());
+    }
+  });
+  $('#fromCurrency').change(function() {
+    convert($('#fromAmount').val(), $('#fromCurrency').val(), $('#toCurrency').val());
+  });
+  $('#toCurrency').change(function() {
+    convert($('#fromAmount').val(), $('#fromCurrency').val(), $('#toCurrency').val());
+  });
+
 });
