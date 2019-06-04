8783cc20dd0e6eefed458eded0c0bc0808584649
Tessa Sullivan and Zach Weintraub <zach@>
2019-06-04 13:14:00 -0700

    Implemented jQuery method of calling API

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { myCode } from './myCode';
 
-$().ready(function(){
-  
-});
+$(document).ready(function() {
+    $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      $('#location').val("");
+      $.ajax({
+        // url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6f4f57f14f2c2b46bc1647f493fb438d`,
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+        },
+        error: function() {
+          $('#errors').text("There was an error processing your request. Please try again.");
+        }
+      });
+    });
+  });
